5891e9a366ff645a2dc9ac1361dc55e2a751b451
Toni Michel <michel@schnapptack.de>
2013-04-24 20:18:48 +0200

    initial checkin

---
+++ schnipp_connector/settings.py
@@ -0,0 +1,4 @@
+def refine_INSTALLED_APPS(original):
+    return  ['schnipp_connector'] + list(original)
+    
+    
---
+++ setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+import os
+import schnipp_connector
+from setuptools import setup, find_packages
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+setup(
+    name='django-productline-schnipp-connector',
+    version='0.1',
+    description='a django-productline feature to include schnipp.js',
+    long_description=read('README.rst'),
+    license='The MIT License',
+    keywords='django, FOSD, FOP, feature-oriented-programming, product-line, javascript, schnipp.js',
+    author='Toni Michel',
+    author_email='toni@schnapptack.de',
+    url="https://github.com/toni/django-productline-schnipp-connector",
+    packages=find_packages(),
+    package_dir={'django_productline': 'django_productline'},
+    package_data={'django_productline': []},
+    include_package_data=True,
+    scripts=[],
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent'
+    ],
+    install_requires=[
+        'django-productline',
+    ]
+)
+
